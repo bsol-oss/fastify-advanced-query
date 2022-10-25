7bd4f1a508d9bd059ee78a02b2e55d3790e7e4e3
jyotipadhi1 <jyotipadhi1@gmail.com>
2022-10-25 16:00:25 +0530

    rollup back

--- rollup.config.js
+++ rollup.config.js
@@ -5,9 +5,7 @@ import external from 'rollup-plugin-peer-deps-external'
 import { terser } from 'rollup-plugin-terser'
 import json from '@rollup/plugin-json'
 
-const packageJSON = await import('./package.json', {
-    assert: { type: 'json' },
-})
+import packageJSON from './package.json'
 const input = './index.js'
 
 export default [
