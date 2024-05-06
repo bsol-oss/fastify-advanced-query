64de49a3bd189cff822f062bb6cbf373d2c8234c
Ross Chiu <18388411+screw123@users.noreply.github.com>
2024-05-07 02:58:33 +0800

    update with latest version

--- rollup.config.js
+++ rollup.config.js
@@ -1,36 +1,35 @@
 import babel from '@rollup/plugin-babel'
 import commonjs from '@rollup/plugin-commonjs'
 import resolve from '@rollup/plugin-node-resolve'
-import external from 'rollup-plugin-peer-deps-external'
 import { terser } from 'rollup-plugin-terser'
 import json from '@rollup/plugin-json'
 
 import packageJSON from './package.json'
 const input = './index.js'
 
-export default [
-    {
-        input,
-        output: [
-            {
-                file: packageJSON.main,
-                format: 'cjs'
-            },
-            {
-                file: packageJSON.module,
-                format: 'es'
-            }
-        ],
-        plugins: [
-            external(),
-            babel({
-                exclude: 'node_modules/**',
-                babelHelpers: 'bundled'
-            }),
-            json(),
-            resolve(),
-            commonjs(),
-            terser()
-        ]
-    }
-]
+export default {
+    input,
+    output: [
+        {
+            file: packageJSON.main,
+            format: 'umd',
+            name: 'index'
+        },
+        {
+            file: packageJSON.module,
+            format: 'esm'
+        }
+    ],
+    plugins: [
+        babel({
+            exclude: 'node_modules/**',
+            babelHelpers: 'bundled'
+        }),
+        json(),
+        resolve(),
+        commonjs(),
+        terser()
+    ],
+    external: [...Object.keys(packageJSON.peerDependencies)]
+}
+
